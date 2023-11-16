e876067dcd38141fc9e92a2244a854d6b3d4f70b
jagLas <jglasher@gmail.com>
2023-11-16 12:57:05 -0500

    Dice Roller Func

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -8,8 +8,21 @@ interface DiceButtonProps extends ControlProps {
     num: number
 }
 
+function rollDice (num: number) {
+    const results: number[] = [];
+    let rolls = 0;
+    while (rolls < num) {
+        const result = Math.floor(Math.random() * (6) + 1);
+        results.push(result)
+        rolls++;
+    }
+
+    return results
+}
+
 function DiceButton ({num, setResult}: DiceButtonProps) {
     const onClickHandler = () => {
+        console.log(rollDice(num))
         setResult(num)
     }
 
